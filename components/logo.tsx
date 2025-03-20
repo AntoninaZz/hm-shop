bba3cd011b565563c3c4c57db4cab0f8b3e661cd
AntoninaZz <zdanton02@gmail.com>
2025-03-20 11:58:29 +0200

    add margin to logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export const Logo = () => {
     return (
         <Link href='/'>
-            <Image src='/logo.jpg' alt='logo' width={80} height={80} className="rounded-full" />
+            <Image src='/logo.jpg' alt='logo' width={75} height={75} className="rounded-full m-4" />
         </Link>
     );
 }
