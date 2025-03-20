47bb5dc78eba5989f3ffa37039a71732a8a59559
AntoninaZz <zdanton02@gmail.com>
2025-03-20 11:50:26 +0200

    unify logo

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
-import Image from "next/image";
+import { Logo } from "./logo";
 
 export const Footer = () => {
     return <footer>
         <div>
-            <Link href='/'>
-                <Image src='/logo.jpg' alt='logo' width={120} height={120} />
-            </Link>
+            <Logo />
             <div>Welcome to our cozy world of handmade crochet toys! 💙💛 Each toy in our shop is crafted with love and care to bring joy and warmth. We create adorable animals, charming characters, and unique gifts that will become cherished friends for both little ones and grown-ups.</div>
             <div>
                 <Link href='/'>Home</Link>
