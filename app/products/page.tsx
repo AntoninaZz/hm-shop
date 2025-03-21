0f24d8668f0ae33f97710092c4e2052f6727cccb
AntoninaZz <zdanton02@gmail.com>
2025-03-21 13:08:01 +0200

    add products page contents

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,12 @@
+import { Filter } from "@/components/filter";
+import { ProductList } from "@/components/productList";
+
 export default function ProductsPage() {
-    return <div>products list here</div>;
+    return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <Filter />
+        <h1 className="mt-12 text-xl font-semibold">Products</h1>
+        <ProductList />
+    </div>
+);
 }
