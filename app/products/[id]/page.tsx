c05bf62d8ae5cf74a9c2f0433d1f17ab943f751c
AntoninaZz <zdanton02@gmail.com>
2025-03-22 09:07:24 +0200

    add customize product component

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { Add } from "@/components/Add";
+import { CustomizeProduct } from "@/components/CustomizeProduct";
 import { ProductImages } from "@/components/ProductImages";
 
 export default function ProductPage() {
@@ -6,7 +8,27 @@ export default function ProductPage() {
             <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
                 <ProductImages />
             </div>
-            <div className="w-full lg:w-1/2 flex flex-col gap-6">text</div>
+            <div className="w-full lg:w-1/2 flex flex-col gap-6">
+                <h1 className="text-4xl font-medium">Product Name</h1>
+                <p>Description Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis molestias corrupti vero blanditiis reprehenderit soluta libero ducimus dolor earum labore tempora dolores numquam eum tenetur, explicabo odit. A, blanditiis pariatur?</p>
+                <hr className="text-[var(--color-light-beige)]" />
+                <div className="flex items-center gap-4">
+                    <h3 className="text-xl text-[var(--color-powder-pink)] line-through">49$</h3>
+                    <h2 className="font-medium text-2xl">45$</h2>
+                </div>
+                <hr className="text-[var(--color-light-beige)]" />
+                <CustomizeProduct />
+                <Add />
+                <hr className="text-[var(--color-light-beige)]" />
+                <div className="text-sm">
+                    <h4 className='font-medium mb-4 uppercase'>Description</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore ut repellendus illo placeat nesciunt sequi. Accusantium, nam iste rem distinctio ex aspernatur hic! Ab vel recusandae nostrum harum dicta. Officia.</p>
+                </div>
+                <div className="text-sm">
+                    <h4 className='font-medium mb-4 uppercase'>Product Info</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore ut repellendus illo placeat nesciunt sequi. Accusantium, nam iste rem distinctio ex aspernatur hic! Ab vel recusandae nostrum harum dicta. Officia.</p>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ components/CustomizeProduct.tsx
@@ -0,0 +1,22 @@
+export const CustomizeProduct = () => {
+    return (
+        <div className="flex flex-col gap-6">
+            <h4 className="font-medium">Choose a color</h4>
+            <ul className="flex items-center gap-3">
+                <li className="w-8 h-8 rounded-full ring-1 ring-[var(--color-light-beige)] cursor-pointer relative bg-[var(--color-powder-pink)]">
+                    <div className="absolute w-10 h-10 rounded-full ring-2 ring-[var(--color-light-beige)] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                </li>
+                <li className="w-8 h-8 rounded-full ring-1 ring-[var(--color-light-beige)] cursor-pointer relative bg-[var(--color-muted-green)]"></li>
+                <li className="w-8 h-8 rounded-full ring-1 ring-[var(--color-light-beige)] cursor-not-allowed relative bg-white">
+                <div className="absolute w-10 h-[2px] bg-[var(--color-powder-pink)] rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                </li>
+            </ul>
+            <h4 className="font-medium">Choose a size</h4>
+            <ul className="flex items-center gap-3">
+                <li className="ring-1 ring-[var(--color-muted-green)] text-[var(--color-muted-green)] rounded-md py-1 px-4 text-sm cursor-pointer">Small</li>
+                <li className="ring-1 ring-[var(--color-muted-green)] text-white bg-[var(--color-muted-green)] rounded-md py-1 px-4 text-sm cursor-pointer">Medium</li>
+                <li className="ring-1 ring-[var(--color-muted-green)] text-[var(--color-muted-green)] rounded-md py-1 px-4 text-sm contrast-150 cursor-not-allowed">Large</li>
+            </ul>
+        </div>
+    );
+}
