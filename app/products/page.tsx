96f579fb055048853d9d628352d9b59521ad1828
AntoninaZz <zdanton02@gmail.com>
2025-03-25 14:06:37 +0200

    add search bar for small screen

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,12 @@
 import { Filter } from "@/components/Filter";
 import { ProductList } from "@/components/ProductList";
+import { SearchBar } from "@/components/SearchBar";
 
 export default function ProductsPage() {
     return (
         <div className="page-padding">
             <Filter />
+            <div className="h-8 w-full py-4 md:hidden"><SearchBar /></div>
             <h1 className="mt-12 text-xl font-semibold">Products</h1>
             <ProductList />
         </div>
