83057cd7b5cb7bea5f6e58bc1aa0c6fecdd42db1
AntoninaZz <zdanton02@gmail.com>
2025-03-25 12:40:57 +0200

    remove unnecessary link

--- components/links.tsx
+++ components/links.tsx
@@ -5,6 +5,5 @@ export const Links = () => {
         <Link href='/'>Home</Link>
         <Link href='/products'>Shop</Link>
         <Link href='/about'>About</Link>
-        <Link href='/contact'>Contact Us</Link>
     </>);
 }
