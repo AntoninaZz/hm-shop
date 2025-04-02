e0cf0aa04041e8f5515c48639ea78b95d38ebe61
AntoninaZz <zdanton02@gmail.com>
2025-04-02 13:31:53 +0300

    fetch products

--- components/productItem.tsx
+++ components/productItem.tsx
@@ -1,19 +1,25 @@
 import Link from "next/link";
 import Image from "next/image";
 import { AddToCartBtn } from "./AddToCartBtn";
-
-export const ProductItem = () => {
+interface ProductItemProps {
+    id: string;
+    name: string;
+    price: number;
+    numberInStock: number;
+    media: string[];
+}
+export const ProductItem: React.FC<ProductItemProps> = ({ id, name, price, numberInStock, media }) => {
     return (
-        <Link href='products/product' className="w-full flex flex-col gap-4 md:w-[45%] lg:w-[22%]">
+        <Link href={`products/${id}`} className="w-full flex flex-col gap-4 md:w-[45%] lg:w-[22%]">
             <div className="relative w-full h-80">
-                <Image src='/slide2.jpg' alt='product' fill sizes="25vw" className="absolute object-cover rounded-md z-1 hover:opacity-0 transition-opacity ease-in duration-500" />
-                <Image src='/slide3.jpg' alt='product' fill sizes="25vw" className="absolute object-cover rounded-md" />
+                <Image src={media[0]} alt={name} fill sizes="25vw" className="absolute object-cover rounded-md z-1 hover:opacity-0 transition-opacity ease-in duration-500" />
+                <Image src={media[1]} alt={name} fill sizes="25vw" className="absolute object-cover rounded-md" />
             </div>
             <div className="flex justify-between gap-4">
-                <span className="font-medium">Product Name</span>
-                <span className="font-semibold">45$</span>
+                <span className="font-medium text-nowrap overflow-hidden text-ellipsis">{name}</span>
+                <span className="font-semibold">{price}$</span>
             </div>
-            <div className="text-sm text-[var(--color-muted-green)]">Description</div>
+            <div className="text-sm text-[var(--color-muted-green)]">{numberInStock > 0 ? 'available' : 'unavailable'}</div>
             <AddToCartBtn />
         </Link>
     );
--- components/productList.tsx
+++ components/productList.tsx
@@ -1,12 +1,11 @@
+import { getProducts } from "@/lib/action";
 import { ProductItem } from "./ProductItem";
 
-export const ProductList = () => {
+export const ProductList = async () => {
+    const products = await getProducts();
     return (
         <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
+            {products.map((product: ProductType) => <ProductItem key={product._id} id={product._id} name={product.name} price={product.price} numberInStock={product.numberInStock} media={product.media} />)}
         </div>
     );
 }
