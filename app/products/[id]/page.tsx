27b537603a8ec284c1795e668fde8fc1302bc910
AntoninaZz <zdanton02@gmail.com>
2025-09-29 15:25:01 +0300

    add related products

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,10 +1,18 @@
 import ProductDetails from "@/components/ProductDetails";
-import { getProductDetails } from "@/lib/actions/action";
+import { ProductList } from "@/components/ProductList";
+import { getProductDetails, getRelatedProducts } from "@/lib/actions/action";
 
 const ProductPage = async ({ params }: { params: { id: string } }) => {
     const productDetails: ProductType = await getProductDetails(params.id);
+    const relatedProducts = await getRelatedProducts(params.id);
     return (
-        <ProductDetails productDetails={productDetails} />
+        <>
+            <ProductDetails productDetails={productDetails} />
+            <div className="mt-24 page-padding">
+                <h1 className="text-2xl">Related Products</h1>
+                <ProductList products={relatedProducts} />
+            </div>
+        </>
     );
 }
 
