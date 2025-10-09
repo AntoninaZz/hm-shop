a18bc8add42173b90377fab8edc20600ec5bab84
AntoninaZz <zdanton02@gmail.com>
2025-10-09 14:18:04 +0300

    add successful payment page

---
+++ app/successful_payment/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const SuccessfulPaymentPage = () => {
+    return (
+        <div className='h-screen max-h-[400px] flex flex-col justify-center items-center gap-5 page-padding'>
+            <p className='text-xl'>Successful Payment</p>
+            <p className='text-[var(--color-muted-green)] text-center'>Thank you for shopping at our store!
+                <br />We will send you your order as soon as possible.</p>
+            <Link href='/'>
+                <button className="rounded-md py-3 px-4 bg-[var(--color-olive-gray)] hover:bg-[var(--color-muted-green)] cursor-pointer text-[var(--background)]">Back to shopping</button>
+            </Link>
+        </div>
+    )
+}
+
+export default SuccessfulPaymentPage;
