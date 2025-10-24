5ec4f3bf04ce43c181a8ef0f833e4032ad0670b3
AntoninaZz <zdanton02@gmail.com>
2025-10-24 15:57:29 +0300

    fixing path @ resolve

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,19 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'res.cloudinary.com'
+        hostname: 'res.cloudinary.com',
       },
     ],
-  }
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 export default nextConfig;
