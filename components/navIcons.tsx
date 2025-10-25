7ce05e558823879e28fd81bf145ea36aef534b79
AntoninaZz <zdanton02@gmail.com>
2025-10-25 17:01:47 +0300

    fix sign in className error

--- components/navIcons.tsx
+++ components/navIcons.tsx
@@ -30,8 +30,12 @@ export const NavIcons = () => {
             {isProfileOpen && (
                 <div className="absolute p-4 rounded-md top-12 right-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-1 bg-white flex flex-col items-center justify-between gap-4">
                     <SignedOut>
-                        <SignInButton className="w-32 rounded-3xl ring-1 ring-[var(--color-olive-gray)] py-2 px-4 text-xs hover:bg-[var(--color-olive-gray)] hover:text-white cursor-pointer text-center" />
-                        <SignUpButton className="w-32 rounded-3xl bg-[var(--color-olive-gray)] text-white py-2 px-4 text-xs hover:bg-[var(--color-muted-green)] cursor-pointer text-center" />
+                        <SignInButton mode="modal">
+                            <button className="w-32 rounded-3xl ring-1 ring-[var(--color-olive-gray)] py-2 px-4 text-xs hover:bg-[var(--color-olive-gray)] hover:text-white cursor-pointer text-center">Sign in</button>
+                        </SignInButton>
+                        <SignUpButton mode="modal">
+                            <button className="w-32 rounded-3xl bg-[var(--color-olive-gray)] text-white py-2 px-4 text-xs hover:bg-[var(--color-muted-green)] cursor-pointer text-center">Sign up</button>
+                        </SignUpButton>
                         <div className='text-xs text-center text-[var(--color-muted-green)]'>You can log in into existing account or sign up if you {"don't"} have one</div>
                     </SignedOut>
                 </div>
